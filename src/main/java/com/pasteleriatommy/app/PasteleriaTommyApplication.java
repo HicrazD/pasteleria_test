d5c612de22e8945bff6eefb8aa31d978a65311d0
Dozy <carlos@devhive.mx>
2023-04-27 14:47:22 -0600

    primer commit, configuracion de postgresql

---
+++ src/main/java/com/pasteleriatommy/app/PasteleriaTommyApplication.java
@@ -0,0 +1,13 @@
+package com.pasteleriatommy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteleriaTommyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteleriaTommyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pasteleriatommy/app/PasteleriaTommyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasteleriatommy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteleriaTommyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
