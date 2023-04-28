eb55841cca53a3f3914568eaee9370358741e15b
Dozy <carlos@devhive.mx>
2023-04-27 23:42:21 -0600

    Se quito una libreria no usable

--- src/main/java/com/pasteleriatommy/app/services/productos/ProductoService.java
+++ src/main/java/com/pasteleriatommy/app/services/productos/ProductoService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import com.pasteleriatommy.app.entities.Producto;
 import com.pasteleriatommy.app.exepciones.ResourceNotFoundException;
